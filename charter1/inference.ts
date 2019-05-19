7f0dbfe81a9dcd8a4482cb98af9b84b0d87c27ab
helloforrestworld@163.com <helloforrestworld@163.com>
2019-05-19 23:23:15 +0800

    add inference

---
+++ charter1/inference.ts
@@ -0,0 +1,37 @@
+// 类型推断
+{
+  let x = 3
+  function a(a = 3) {
+
+  }
+}
+
+// 通用类型
+{
+  let x = [0, 1, 'string', null]
+  x = ['string']
+  x = [2]
+  // x = [true]
+  x = [null]
+  x = [undefined]
+
+  // 由于最终的通用类型取自候选类型，有些时候候选类型共享一个公共结构，但是却没有一个类型能做为所有候选类型的超级类型
+
+  class Animal {
+    numLegs: number
+  }
+
+  class Bee extends Animal {
+  }
+
+  class Lion extends Animal {
+  }
+
+  let zoo = [new Bee(), new Lion()]
+  zoo = [new Animal()]
+}
+
+// 上下文类型
+{
+
+}
